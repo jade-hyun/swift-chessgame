7606ad61dc2df2e5f253dd3e9f61f2c774b9e5b5
jade.h <jade.h@kakaocorp.com>
2022-06-23 15:35:04 +0900

    pair01a 짝프로그래밍 준비

---
+++ pair01a/pair01a/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  pair01a
+//
+//  Created by jade.h on 2022/06/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+func numberOne() -> Int {
+    return 1
+}
---
+++ pair01a/pair01atest/pair01atest.swift
@@ -0,0 +1,16 @@
+//
+//  pair01atest.swift
+//  pair01atest
+//
+//  Created by jade.h on 2022/06/23.
+//
+
+import XCTest
+
+class pair01atest: XCTestCase {
+
+    func test숫자1테스트() {
+        XCTAssertEqual(numberOne(), 1)
+    }
+
+}
